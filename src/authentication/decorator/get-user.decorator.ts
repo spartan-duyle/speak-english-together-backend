4bd332e021b2f7211412cc174964821d050ff5f8
spartan-duyle <duy.le@c0x12c.com>
2024-04-27 11:12:06 +0700

    api login, verify account, refresh token

--- src/authentication/decorator/get-user.decorator.ts
+++ src/authentication/decorator/get-user.decorator.ts
@@ -1,11 +1,14 @@
-// import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
+import { UserPayload } from '../types/user.payload';
 
-// export const GetUser = createParamDecorator(
-//   (data: string | undefined, ctx: ExecutionContext) => {
-//     const request: Express.Request = ctx.switchToHttp().getRequest();
-//     if (data) {
-//       return request.user[data];
-//     }
-//     return request.user;
-//   },
-// );
+export const GetUser = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+    if (!request.user) throw new BadRequestException();
+    return request.user as UserPayload;
+  },
+);
