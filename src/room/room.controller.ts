3f67b6416ae7276d3ba658d87401e410ee48d69a
spartan-duyle <duy.le@c0x12c.com>
2024-05-04 01:34:47 +0700

    create a live room api

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, UseGuards } from "@nestjs/common";
 import { RoomService } from './room.service';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { APISummaries } from 'src/helpers/helpers';
 import { UserGuard } from 'src/authentication/guard/auth.guard';
 import { VerifiyGuard } from 'src/authentication/guard/verify.guard';
 import { UserPayload } from 'src/authentication/types/user.payload';
 import { GetUser } from 'src/authentication/decorator/get-user.decorator';
 import CreateRoomDto from './dto/createRoom.dto';
+import VideoSDKTokenResponse from './response/videoSDKToken.response';
+import CreateRoomResponse from './response/createRoom.response';
 
 @Controller('room')
 @ApiTags('rooms')
@@ -17,16 +19,21 @@ export class RoomController {
   @ApiBearerAuth()
   @ApiOperation({ summary: APISummaries.USER })
   @UseGuards(UserGuard, VerifiyGuard)
-  @ApiBearerAuth()
-  async generateVideoSDKToken() {
+  @ApiOkResponse({ type: VideoSDKTokenResponse })
+  async generateVideoSDKToken(): Promise<VideoSDKTokenResponse> {
     return await this.roomService.generateVideoSDKToken();
   }
 
   @Post('')
+  @HttpCode(201)
   @ApiBearerAuth()
   @ApiOperation({ summary: APISummaries.USER })
   @UseGuards(UserGuard, VerifiyGuard)
-  async createRoom(@GetUser() user: UserPayload, @Body() data: CreateRoomDto) {
+  @ApiOkResponse({ type: CreateRoomResponse })
+  async createRoom(
+    @GetUser() user: UserPayload,
+    @Body() data: CreateRoomDto,
+  ): Promise<CreateRoomResponse> {
     return await this.roomService.createRoom(user, data);
   }
 }
