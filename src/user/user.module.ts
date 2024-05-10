4098cb2db5783c001735392253e73b47f333b002
spartan-duyle <duy.le@c0x12c.com>
2024-05-11 02:30:21 +0700

    get user profile, update profile, change users password

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import UserService from './user.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [ConfigModule, PrismaModule],
-  controllers: [],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
 import { plainToInstanceCustom } from 'src/helpers/helpers';
 import { UserModel } from './model/user.model';
-import { UserNotFoundException } from './exceptions/userNotFound.exception';
 import { PrismaService } from 'src/prisma/prisma.serivce';
 import { UserStatus } from './enum/userStatus.enum';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserProfileResponse } from './response/userProfile.response';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { ChangePasswordDto } from './dto/changePassword.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export default class UserService {
@@ -12,7 +19,7 @@ export default class UserService {
   async getByEmail(email: string): Promise<UserModel> {
     const user = this.prismaService.user.findUnique({ where: { email } });
     if (!user) {
-      throw new UserNotFoundException();
+      throw new NotFoundException('User not found');
     }
     return plainToInstanceCustom(UserModel, user);
   }
@@ -21,6 +28,60 @@ export default class UserService {
     return this.prismaService.user.update({
       where: { email },
       data: { status: UserStatus.ACTIVE },
+    });
+  }
+
+  async userProfile(id: number): Promise<UserProfileResponse> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: id, deleted_at: null },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return plainToInstanceCustom(UserProfileResponse, user);
+  }
+
+  async updateUserProfile(id: number, data: UpdateUserDto) {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+    const updatedUser = this.prismaService.user.update({
+      where: { id },
+      data: {
+        full_name: data.full_name,
+        description: data.description,
+        avatar_url: data.avatar_url,
+        level: data.level,
+        nationality: data.nationality,
+      },
+    });
+
+    return plainToInstanceCustom(UserProfileResponse, updatedUser);
+  }
+
+  async changePassword(id: number, data: ChangePasswordDto): Promise<void> {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (data.new_password !== data.confirm_password) {
+      throw new BadRequestException('Password does not match');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.new_password, 10);
+
+    await this.prismaService.user.update({
+      where: { id: id },
+      data: {
+        password: hashedPassword,
+        updated_at: new Date(),
+      },
     });
   }
 }
