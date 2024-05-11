f5e8751cfa86dad5e3d7079d4bc42906e3ab65a9
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2024-05-11 18:33:01 +0700

    add content to topic (#15)

---
+++ src/database/migrations/init_topic_data/migration.sql
@@ -0,0 +1,98 @@
+INSERT INTO "Topic" ("name", "image", "content")
+VALUES
+    ('Job Career', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715424389791_job.png', 'A: What is your desired job?
+B: Well, my dream changes in accordance with age. Up to now, I would like to be a great English teacher.
+A: Is that a demanding job?
+B: Yes, I think so although people suppose that being a teacher is easy and boring.
+A: What challenge do you have when doing that job?
+B: I have some problems with classroom management. Maybe I have to improve that skill a lot in order to be a good teacher.
+A: Is it hard to make your dream come true?
+B: Every job needs time and effort, and a teacher does as well. Apart from the academic knowledge I was taught in university, I have to learn many other soft skills.
+A: Why do you choose that job?
+B: I really admired my high school English teacher, and I want to be like her.
+A: What requirements and characteristics do you need to do that job?
+B: Let’s see. You need to be patient, thoughtful, and sympathetic.
+A: Does the job have something to do with your major at university?
+B: Sure. I studied English Linguistics and Literature, majored in English teaching. The major does support me a lot in my career path.
+A: What does society think about your job?
+B: I’m not quite sure, but I’m proud to be a teacher, an honorable job.
+A: Do you make much money with that job?
+B: Not at all. Teaching is one of the lowest paying jobs in my country.
+A: Have you ever thought of leaving your job?
+B: I haven’t thought of that before, but I believe in what I’m doing right now.
+A: What do you think about the future of your job?
+B: I think the job will be more and more important in the future, and I hope I can contribute to society by teaching English.
+A: Oh, I see. Thank you for sharing your thoughts with me. Good luck with your job.
+B: Thank you. I hope you will find your dream job soon.'),
+    ('Television program', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715425163274_tv-show.jpeg', 'A: What is your favorite TV program?
+B: Well, I watch a lot, but the one I like best is “How’s it made”.
+A: Is that an international TV program?
+A: Yes, it is. It’s available in Canada, Europe, Australia, New Zealand, Southeast Asia, and so on.
+A: What is the TV program about?
+B: Its name somehow describes the content, which is about the process of making things like lipsticks, balls, candy, toys, chocolate and so on.
+A: How often do you watch that TV program?
+B: Almost every day after dinner.
+A: Who watches that program with you?
+A: My family watches it together. My younger brother can’t wait to turn on the TV.
+B: What channel is it on?
+A: A very common one, Discovery channel, which focuses on popular science, technology, and history.
+B: Why do you like that TV program?
+A: I love learning new things, especially about how everything is produced. I would love to run a business about hand-made cosmetics.
+A: How does that TV program change you?
+B: I know more about the world around me. It also supports me in my career path to make my dream come true.
+A: Who is its target audience?
+B: It particularly aims at families and younger audiences.
+A: Would you recommend that TV program to your friends?
+B: Yes, of course. They would be crazy about the program, I bet.'),
+('Sport', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715244753417_icons8-basketball-64.png', 'Jame: What sport do you like?
+Jonny: I like playing badminton.
+Jame: Is it easy to play that sport?
+Jonny: Yes, it’s pretty easy to play.
+Jame: Is that sport popular in your country?
+Jonny: Yes, it is.
+Jame: How long have you been practicing that sport?
+Jonny: I have been practicing it for 5 years.
+Jame: Who do you play sports with?
+Jonny: I play badminton with my friends, sometimes with my brother.
+Jame: How often do you play that sport?
+Jonny: I play badminton every weekend.
+Jame: What benefits can you get from that sport?
+Jonny: It helps strengthen my muscles because while playing, I have to move continuously. It is good to burn calories as well.
+Jame: Do you like watching football? Online or offline?
+Jonny: Yes, I do. I prefer watching football offline and online. Going to the stadium, shouting and cheering are good to release stress.
+Jame: What is your favorite football team?
+Jonny: I like the Manchester United Football Club, also known as “The Red Devils”.
+Jame: Why is sport important?
+Jonny: Sports are sources of recreation. People can learn how to encourage team spirit when they play sports, too.'
+),
+('Introduction', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715425821443_introduction.jpeg', 'Laura: Hi, I noticed you''re new here. Could you tell me a bit about yourself?
+Tom: Of course, Laura! I''m Tom, from Dallas, Texas. I''m currently studying Environmental Science.
+Laura: That''s fantastic! What got you interested in Environmental Science?
+Tom: I''ve always loved nature, and I wanted to find ways to protect our environment. What about you, Laura? What are your hobbies or interests?
+Laura: I love reading, especially fiction. It allows me to escape into different worlds. How do you spend your free time?
+Tom: I enjoy photography. It''s a great way for me to capture moments in life. Do you have a favorite food or music genre?
+Laura: I absolutely love Italian food and I''m a big fan of jazz music. And you?
+Tom: I''m really into rock music and my go-to comfort food would be homemade burgers.
+Laura: That sounds wonderful, Tom! It was great getting to know you better.
+Tom: Likewise, Laura! I''m looking forward to our next conversation.'),
+('A day in the life', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715425902368_life.webp', 'Emma: Hi, I''ve been curious about your skincare routine. Mind sharing some tips?
+John: Of course, Emma! In the morning, I cleanse with a gentle foaming wash.
+Emma: Oh, that sounds interesting! How about nighttime? Do you have a specific routine?
+John: Yes, before bed, I apply a nourishing moisturizer to keep my skin soft.
+Emma: That''s great! How often do you scrub your skin for a healthy glow?
+John: I scrub twice a week to remove dead skin cells and promote renewal.
+Emma: I see. One more thing, do you use sunscreen daily for sun protection?
+John: Absolutely! Sunscreen is a must to shield my skin from harmful UV rays.
+Emma: Thanks for sharing, John! I''ll give those tips a try.
+John: You''re welcome, Emma! Remember, consistency is key. Good luck with it'),
+('Travel', 'https://storage.googleapis.com/speaking-english-together.appspot.com/images/1715426008352_travel.png', 'Anna: Hey, have you been to Vietnam? I heard it''s beautiful!
+Mike: Yes, Anna! Ha Long Bay is stunning with its limestone rock formations.
+Anna: Oh, that sounds amazing! Any other famous places to visit?
+Mike: Definitely! Hoi An is a charming ancient town with colorful lanterns.
+Anna: Wow, I love that! Are there any beautiful beaches in Vietnam?
+Mike: Yes, Nha Trang offers white sandy beaches and crystal-clear waters.
+Anna: That sounds like a dream! Any more must-see places you recommend?
+Mike: You should visit Sapa for its breathtaking rice terraces and mountain views.
+Anna: Thanks, Mike! I''ll add these destinations to my travel bucket list.
+Mike: No problem, Anna! Vietnam is fantastic. Enjoy your trip!'
+);
