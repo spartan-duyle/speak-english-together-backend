d4bccee5c5d280ec34013740942ff09aea441a1a
spartan-duyle <duy.le@c0x12c.com>
2024-05-15 09:22:30 +0700

    user list api

--- src/features/internals/room/response/listRoom.response.ts
+++ src/features/internals/room/response/listRoom.response.ts
@@ -1,4 +1,4 @@
-import { PaginatedOutputResponse } from '../../../../common/helpers/responses/paginatedOutput.response';
+import { PaginatedOutputResponse } from '@/common/helpers/responses/paginatedOutput.response';
 import { RoomResponse } from './room.response';
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
