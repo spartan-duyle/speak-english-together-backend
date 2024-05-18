7bb94a87c29a85899e79f156ba683e5b1e154c52
spartan-duyle <duy.le@c0x12c.com>
2024-05-18 08:39:45 +0700

    update api following

--- src/features/internals/user/user.module.ts
+++ src/features/internals/user/user.module.ts
@@ -4,11 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { UserController } from './user.controller';
 import UserRepository from '@/features/internals/user/user.repository';
+import FollowerRepository from '@/features/internals/follower/follower.repository';
 
 @Module({
   imports: [ConfigModule, PrismaModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, FollowerRepository],
   exports: [UserService, UserRepository],
 })
 export class UserModule {}
