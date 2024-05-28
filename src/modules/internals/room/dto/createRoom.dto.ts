7b9f658ef9a403f461dbfb90a60024263d8f8e78
spartan-duyle <duy.le@c0x12c.com>
2024-05-28 21:08:44 +0700

    rebase main

--- src/modules/internals/room/dto/createRoom.dto.ts
+++ src/modules/internals/room/dto/createRoom.dto.ts
@@ -16,7 +16,7 @@ export default class CreateRoomDto {
   @ApiProperty({ type: Number, required: true, nullable: true })
   @IsNumber()
   @IsOptional()
-  collectionId: number;
+  topicId: number;
 
   @ApiProperty({ type: Boolean, required: true, nullable: true })
   @IsOptional()
