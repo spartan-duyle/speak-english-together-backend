ecbfc016e1f9e759901d4c44b60ea7efdc608258
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2024-06-07 23:46:54 +0700

    generate sentences in room (#38)

--- src/modules/internals/room/room.module.ts
+++ src/modules/internals/room/room.module.ts
@@ -8,11 +8,12 @@ import { FirebaseModule } from '../../externals/firebase/firebase.module';
 import { VideoSDKModule } from '../../externals/videoSDK/videoSDK.module';
 import RoomRepository from '@/modules/internals/room/room.repository';
 import { TopicModule } from '@/modules/internals/topic/topic.module';
+import { OpenaiModule } from '@/modules/internals/openai/openai.module';
 
 @Module({
   controllers: [RoomController],
   providers: [RoomService, PrismaService, JwtService, RoomRepository],
   exports: [RoomService],
-  imports: [RoomMemberModule, FirebaseModule, VideoSDKModule, TopicModule],
+  imports: [RoomMemberModule, FirebaseModule, VideoSDKModule, TopicModule, OpenaiModule],
 })
 export class RoomModule {}
